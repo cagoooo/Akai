85d6d88aced49ecd6c4870b33bdc24834a7fab45
ipad4 <21751599-ipad4@users.noreply.replit.com>
2025-01-06 13:00:06 +0000

    Agent query: Is the QueryClient error resolved and is the progress dashboard visible now?
    Move QueryClientProvider to App component.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/3ffbb362-7f42-446d-9993-60676029ba6a/c900ac21-d434-45df-8cd0-3ae34a2d1ad3.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,18 +4,22 @@ import { HelpProvider } from "@/components/HelpProvider";
 import { Toaster } from "@/components/ui/toaster";
 import { BreakpointIndicator } from "@/components/BreakpointIndicator";
 import { PageTransition } from "@/components/PageTransition";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 
 function App() {
   return (
-    <HelpProvider>
-      <PageTransition>
-        <Switch>
-          <Route path="/" component={Home} />
-        </Switch>
-      </PageTransition>
-      <Toaster />
-      <BreakpointIndicator />
-    </HelpProvider>
+    <QueryClientProvider client={queryClient}>
+      <HelpProvider>
+        <PageTransition>
+          <Switch>
+            <Route path="/" component={Home} />
+          </Switch>
+        </PageTransition>
+        <Toaster />
+        <BreakpointIndicator />
+      </HelpProvider>
+    </QueryClientProvider>
   );
 }
 
