303288ca79b43ba1acec4873a7b7d0c593190766
ipad4 <21751599-ipad4@users.noreply.replit.com>
2025-01-06 07:53:50 +0000

    Agent query: Is the application loading without any translation errors now?
    Remove TranslationProvider from App component
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/3ffbb362-7f42-446d-9993-60676029ba6a/e75ead35-5a96-4d7e-acc0-48ffd848c0e3.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,16 @@
 import { Switch, Route } from "wouter";
 import { Home } from "@/pages/Home";
 import { HelpProvider } from "@/components/HelpProvider";
-import { TranslationProvider } from "@/lib/translation";
 import { Toaster } from "@/components/ui/toaster";
 
 function App() {
   return (
-    <TranslationProvider>
-      <HelpProvider>
-        <Switch>
-          <Route path="/" component={Home} />
-        </Switch>
-        <Toaster />
-      </HelpProvider>
-    </TranslationProvider>
+    <HelpProvider>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+      <Toaster />
+    </HelpProvider>
   );
 }
 
