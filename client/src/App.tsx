1b264fc89f3447009d9cfb44b14353d10bf6ed19
ipad4 <21751599-ipad4@users.noreply.replit.com>
2025-01-08 08:15:21 +0000

    Agent query: 請確認現在「你知道嗎？」提示是否會持續顯示，不會因為頁面載入完成而消失？同時確認載入指示器是否正常顯示？
    Move trivia dialog from loading screen to main app and refactor component.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/3ffbb362-7f42-446d-9993-60676029ba6a/55e04c73-fbdb-4444-9859-121ba20d17cf.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,7 @@ import { PageTransition } from "@/components/PageTransition";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Footer } from "@/components/Footer";
+import { TriviaDialog } from "@/components/TriviaDialog";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           </PageTransition>
           <Footer />
         </div>
+        <TriviaDialog />
         <Toaster />
         <BreakpointIndicator />
       </TourProvider>
