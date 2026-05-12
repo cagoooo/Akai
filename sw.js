c1799daa4b5e8c5c0a64e07889ed45a6772c499e
cagoooo <ipad@mail2.smes.tyc.edu.tw>
2026-05-12 12:27:23 +0800

    Updates

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * - Stale While Revalidate: 圖片
  */
 
-const CACHE_VERSION = 'v3.6.21-b930aa6-202605091706';
+const CACHE_VERSION = 'v3.6.22-2c40076-202605121226';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `dynamic-${CACHE_VERSION}`;
 const ASSETS_ARCHIVE = 'assets-archive-v1';
